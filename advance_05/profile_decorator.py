735686b107813850fa1f037e22048de661f9ddee
Jana Kutsko <pasteyourtext@gmail.com>
2022-12-06 00:20:10 +0300

    redo decorator

--- advance_05/profile_decorator.py
+++ advance_05/profile_decorator.py
@@ -1,31 +1,45 @@
 import cProfile
+import io
+import pstats
+import time
 from functools import wraps
 
 
 def profile_deco(func):
     def print_stat():
-        print('Hello')
+        print(*profile_stats)
 
     func.print_stat = print_stat
-    pr = cProfile.Profile()
-    pr.enable()
+    profile_stats = []
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
+        prof = cProfile.Profile()
+        prof.enable()
+        res = func(*args, **kwargs)
+
+        s = io.StringIO()
+        sortby = "cumulative"
+        stats = pstats.Stats(prof, stream=s).sort_stats(sortby)
+        stats.print_stats()
+        profile_stats.append(s.getvalue())
+
+        return res
+
     return wrapper
 
 
 @profile_deco
-def add(a, b):
+def add(a, b, sleep):
+    time.sleep(sleep)
     return a + b
 
 
 if __name__ == '__main__':
-    print(add(1, 2))
-    print(add(1, 2))
-    print(add(1, 2))
-    print(add(1, 2))
+    print(add(1, 2, 0.5))
+    print(add(1, 2, 0.1))
+    print(add(1, 2, 0.3))
+    print(add(1, 2, 0.2))
     add.print_stat()
 
 
